3c0a5e32695d9e85a313fc4d83a432fb8a10aed2
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-09-01 17:00:57 +0300

    animal_quiz 2

--- animal_quiz/main.py
+++ animal_quiz/main.py
@@ -4,9 +4,20 @@ score = 0
 
 def check_guess(guess, answer):
     global score
-    if guess == answer:
-        print("Correct answer")
-        score += 1
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print("Correct answer.")
+            score += 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input("Sorry wrong answer. Try again: ")
+            attempt += 1
+
+    if attempt == 3:
+        print("The correct answer is: " + answer)
 
 
 guess1 = input("Which bear lives at the North Pole? ")
