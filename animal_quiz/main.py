8bb027ca37829b412333baa969d7c8189d953163
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-09-01 17:26:48 +0300

    animal_quiz 5

--- animal_quiz/main.py
+++ animal_quiz/main.py
@@ -3,13 +3,14 @@ score = 0
 
 
 def check_guess(guess, answer):
-    global score
+    global score  # global значит что мы используем глобальную переменную, объявленную выше
     still_guessing = True
     attempt = 0
     while still_guessing and attempt < 3:
         if guess.lower() == answer.lower():
             print("Correct answer.")
-            score = score + 3 - attempt
+            # с каждым неправильным ответом кол-во очков которые можно получить за ответ, уменьшается
+            score = score + 3 - attempt  # максимум 3
             still_guessing = False
         else:
             if attempt < 2:
@@ -31,5 +32,8 @@ guess4 = input("Which one of these is a fish?\n \
 A) Whale\n B) Dolphin\n C) Shark\n D) Squid\n \
 Type A, B, C, or D: ")
 check_guess(guess4, "C")
+
+guess5 = input("Mice are mammals. True or False? ")
+check_guess(guess5, "True")
 
 print("Your score is: " + str(score))
