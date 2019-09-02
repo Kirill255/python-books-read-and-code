2bdbe3b9604787a60cae0eb1b3040876f1ebeed9
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-09-02 15:14:09 +0300

    password_picker 3

--- password_picker/main.py
+++ password_picker/main.py
@@ -12,17 +12,20 @@ adjectives = ["sleepy", "slow", "smelly",
 
 nouns = ["apple", "dinosaur", "ball",
          "toaster", "goat", "dragon",
-         "hammer", "duck", "panda"
+         "hammer", "duck", "panda",
+         "telephone", "banana", "teacher"
          ]
-while True:
-    adjective = random.choice(adjectives)
-    noun = random.choice(nouns)
-    number = random.randrange(0, 100)
-    sign = random.choice(string.punctuation)  # рандомный знак из строки !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-    # print(string.punctuation)
 
-    password = adjective + noun + str(number) + sign
-    print("Your new password is: %s" % password)  # fatpanda56= or fluffyduck16+
+while True:
+    for num in range(3):  # теперь генерируем сразу по 3 пароля
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100)
+        sign = random.choice(string.punctuation)  # рандомный знак из строки !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        # print(string.punctuation)
+
+        password = adjective + noun + str(number) + sign
+        print("Your new password is: %s" % password)  # fatpanda56= or fluffyduck16+
 
     response = input("Would you like another password? Type y or n: ")
     if response == "n":
