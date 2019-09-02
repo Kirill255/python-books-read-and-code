d3b8dd44d3f04fcd80672728d56cda6bbdb0e1a4
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-09-02 20:56:31 +0300

    nine_lives 4

--- nine_lives/main.py
+++ nine_lives/main.py
@@ -1,15 +1,24 @@
 from random import choice
 
-# для удобства у нас все слова из 5 букв
+# (deprecated) для удобства у нас все слова из 5 букв
+# теперь слова могут быть разной длинны
 words = ["pizza", "fairy", "teeth",
          "shirt", "otter", "plane",
-         "brush", "horse", "light"
+         "brush", "horse", "light",
+         "cat", "python", "beautiful"
          ]
 secret_word = choice(words)
-board = list("?????")  # 5шт по кол-ву букв в словах ['?', '?', '?', '?', '?']
+# board = list("?????")  # (deprecated) 5шт по кол-ву букв в словах ['?', '?', '?', '?', '?']
+board = []  # теперь будем динамически заполнять список
 lives = 12
 heart_symbol = u"\u2764"  # ❤
 guessed_word_correctly = False
+
+# заполняем доску, теперь можно загадывать слова разной длинны
+index = 0
+while index < len(secret_word):
+    board.append("?")
+    index += 1
 
 
 # обновляем слово на доске, функция вызывается только если введённая пользователем буква, точно присутствует в слове
@@ -27,8 +36,7 @@ def update_clue(guessed_letter, secret_word, board):
 difficulty = int(input("Choose difficulty (type 1, 2, 3):\n 1 Easy\n 2 Normal\n 3 Hard\n"))
 # если пользователь введёт другое число, тогда число жизней останется равно 12
 if 3 >= difficulty >= 1:
-    lives /= difficulty  # 12/1 = 12.0, 12/2 = 6.0, 12/3 = 4.0
-    lives = int(lives)  # потому что после деления получается float число
+    lives //= difficulty  # 12//1 = 12, 12//2 = 6, 12//3 = 4, целочесленное деление поэтому приводить к int() не надо
 
 while lives > 0:
     print(board)  # распечатываем доску
@@ -43,15 +51,15 @@ while lives > 0:
     # сравниваем побуквенно, если буквы точно есть в слове, обновляем доску
     if guess in secret_word:
         update_clue(guess, secret_word, board)
-
-        # проверяем отгадано ли слово целиком
-        if "".join(board) == secret_word:
-            guessed_word_correctly = True
-            break
     else:
         # инчае буква неверная, вычитаем жизнь
         print("Incorrect. You lose a life")
         lives -= 1
+
+    # проверяем отгадано ли слово целиком
+    if "".join(board) == secret_word:
+        guessed_word_correctly = True
+        break
 
 if guessed_word_correctly:
     print("You won! The secret word was " + secret_word)
